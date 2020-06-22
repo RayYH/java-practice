ea389128e12014b807a1c84fcdeefa8a07f736c2
rayyh <rayyounghong@gmail.com>
2020-06-22 13:22:37 +0800

    add some description about generics

--- src/main/java/com/rayyounghong/core/generics/Generic.java
+++ src/main/java/com/rayyounghong/core/generics/Generic.java
@@ -46,3 +46,26 @@ class Generic<T> {
  * Unbounded wildcard − If a variable can be accessed using Object class method then use an unbound wildcard.
  * No wildcard − If code is accessing variable in both in and out category then do not use wildcards.
  */
+
+/*
+ * Type Erasure Rules:
+ *
+ * Replace type parameters in generic type with their bound if bounded type parameters are used.
+ * Replace type parameters in generic type with Object if unbounded type parameters are used.
+ * Insert type casts to preserve type safety.
+ * Generate bridge methods to keep polymorphism in extended generic types.
+ */
+
+/*
+ * Other Points:
+ *
+ * No Primitive Types.
+ * No Instance: cannot use `T item = new T();`.
+ * No Static Field: cannot use `private static T t;`.
+ * No Cast: https://www.tutorialspoint.com/java_generics/java_generics_no_cast.htm
+ * NO instanceof: https://www.tutorialspoint.com/java_generics/java_generics_no_instanceof.htm
+ * NO Array: cannot use `Box<Integer>[] arrayOfLists = new Box<Integer>[2];` - see type erasure.
+ * No Exception: A generic class is not allowed to extend the Throwable class directly or indirectly.
+ * No Overload: A class is not allowed to have two overloaded methods that can have the same signature after type 
+ * erasure.
+ */
