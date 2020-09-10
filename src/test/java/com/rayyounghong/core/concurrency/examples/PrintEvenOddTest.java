44cb9d5f8069d8f2b0d505356090152c755c1527
rayyh <rayyounghong@gmail.com>
2020-09-10 09:19:54 +0800

    flush outContent

--- src/test/java/com/rayyounghong/core/concurrency/examples/PrintEvenOddTest.java
+++ src/test/java/com/rayyounghong/core/concurrency/examples/PrintEvenOddTest.java
@@ -5,6 +5,7 @@ import com.rayyounghong.core.concurrency.examples.evenandodd.Printer;
 import com.rayyounghong.core.concurrency.examples.evenandodd.TaskEvenOdd;
 import com.rayyounghong.helper.OSEnum;
 import com.rayyounghong.helper.Os;
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -16,8 +17,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author ray
  */
 public class PrintEvenOddTest extends StandardIOTest {
+
     @Test
-    void testWaitNotify() {
+    void testWaitNotify() throws IOException {
+        outContent.flush();
         int maxNumber = 10;
         Printer printer = new Printer();
         ExecutorService executorService = Executors.newFixedThreadPool(2);
@@ -32,9 +35,7 @@ public class PrintEvenOddTest extends StandardIOTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        if (Os.getOperatingSystem() != OSEnum.WINDOWS) {
-            assertEquals("1 2 3 4 5 6 7 8 9 10 ", outContent.toString());
-        }
+        assertEquals("1 2 3 4 5 6 7 8 9 10 ", outContent.toString());
     }
+
 }
